070be14025a4293d4fb285275d648f4506e18066
kkwn02 <kylekwon22@gmail.com>
2022-02-08 01:33:38 -0600

    Add to cart feature

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import requests
-from util.stock import checkStock
+from util.stock import *
 
 s = requests.Session();
 
-response = checkStock(s, 'Bouncle')
-if  response != None:
-    print(response)
+link = 'https://www.ericemanuel.com/collections/products'
+itemLink = 'https://www.ericemanuel.com' + findItemLink(s, link, 'boucle', 'Pink')
+
+if itemLink and checkItemStock(s, itemLink, 'XL'):
+    itemId = getItemId(s, link, 'XL', 'red')
+
+if itemId:
+    print(itemId)
+    statusCode = addToCart(s, itemLink, itemId, 'XL')
+    if statusCode == 200:
+        print('Item added successfully')
+
+
--- util/stock.py
+++ util/stock.py
@@ -1,35 +1,74 @@
 import requests
 from bs4 import BeautifulSoup as soup
+import json
+import re
 
-def checkStock(session, itemName):
-    link = 'https://www.ericemanuel.com/collections/products'
-    link2 = 'https://kith.com/collections/kith'
-    response = session.get(link)
+def findItemLink(session, productsLink, itemName, color):
+    response = session.get(productsLink)
     if response.status_code != 200:
-        return None;
+        return None
     parsed = soup(response.text, "html.parser")
-    # t = parsed.find_all('img', alt='EE® BOUCLE SWEATS')
-
     t = parsed.find_all('a', href=True)
-    # t = parsed.find_all('entry')
-    product_links = []
-    # colors = ['Purple', 'Orange', 'Pink']
-    color = 'Purple'
-    plink = ''
+    plink = None
     for i in t:
-        if i['href'].find('products/products/') != -1:
-            product_links.append(i['href'])
+        if i['href'].find('products/products/') != -1 and i['href'].find(itemName) != -1:
             content = i.find_all('noscript')
             for j in content:
-                # for color in colors:
-                #     if j.find('img')['src'].find(color) != -1:
-                #         product_links.append(color)
                 if j.find('img')['src'].find(color) != -1:
                     plink = i['href']
+    return plink
 
-    # for i in product_links:
-    #     print(i)
-    print(plink)
+def checkItemStock(session, productLink, itemSize):
+    response = session.get(productLink)
+    if response.status_code != 200:
+        return None
+    parsed = soup(response.text, "html.parser")
+    t = parsed.find_all(attrs={'data-value': itemSize})
+
+    for i in t:
+        if len(i.find('input')['class']) > 1:
+            return False
+    return True
+
+def getItemId(session, productsLink, size, color):
+    response = session.get(productsLink)
+    if response.status_code != 200:
+        return None
+    bs = soup(response.text, "html.parser")
+    scripts = bs.find_all('script')
+    itemId = None
+    for script in scripts:
+        if script.text.find('var meta') != -1:
+            sstr = str(script.string)
+            idstr = sstr[sstr.find('var meta'):sstr.find('for (')]
+            jsonvar, trash, jsonstr = idstr.partition(' = ')
+    jsonlist = re.findall(r'\{"id":[0-9]{14}.+?"}',jsonstr)
+    for i in jsonlist:
+        jsonObj = json.loads(i)
+        if jsonObj['public_title'] == size and jsonObj['sku'].lower().find(color) != -1:
+            itemId = (jsonObj['id'])
+    return itemId
+
+
+def addToCart(session, productLink, itemId, size):
+    payload = {
+        'form_type': 'product',
+        # 'utf8': '%E2%9C%93',
+        'Size': size,
+        'id': itemId
+    }
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36',
+        'Accept': 'application/json',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Origin': 'https://www.ericemanuel.com',
+        'Authority': 'www.ericemanuel.com'
+    }
+    url = 'https://www.ericemanuel.com/cart/add.js'
+    response = session.post(url, headers=headers, data=payload)
+    return response.status_code
 
 
 
